8c1ee928e3f3b4f8b7243a7c62e05e96a8e3efeb
daniel <danielmilewski123@gmail.com>
2023-07-06 10:10:42 +0200

    Calculate Reward and claim functions created in Staking contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -27,4 +27,20 @@ contract Staking is Ownable {
         positions[positionIndex] = Position(_amount, block.timestamp, msg.sender, false);
         ++positionIndex;
     }
+
+    function calculateReward(uint256 _id) public view returns(uint256) {
+        uint256 stakingTime = block.timestamp - positions[_id].createdAt;
+        uint256 numberDays = stakingTime / (3600 * 24);
+        uint256 reward = numberDays * 12;
+
+        return reward;
+    }
+
+    function claim(uint256 _id) external {
+        require(positions[_id].createdBy == msg.sender, "not authorized");
+        require(positions[_id].claimed == false, "already claimed");
+
+        positions[_id].claimed = true;
+        token.transfer(msg.sender, positions[_id].amount + calculateReward(_id));
+    }
 }
