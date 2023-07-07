c5cab8cda4ca4525bf626c87ff06e8c68dbead80
daniel <danielmilewski123@gmail.com>
2023-07-07 08:52:21 +0200

    Test for claiming rewards created

--- test/Staking.test.js
+++ test/Staking.test.js
@@ -1,5 +1,6 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
 const getErc20 = async (deployer) => {
     const erc20_ = await ethers.getContractFactory("MyToken");
@@ -31,6 +32,41 @@ describe("Staking", () => {
             await erc20.connect(admin).transfer(staker.address, 1000);
             await erc20.connect(staker).increaseAllowance(contract.target, 1000);
             await contract.connect(staker).stake(1000);
+
+            const position = await contract.positions(0);
+            expect(position.amount).to.eq("1000");
+            expect(position.createdBy).to.eq(staker.address);
+        })
+
+        it("reverts when no erc20 owned", async () => {
+            const [admin, staker] = await ethers.getSigners();
+            const Staking = await ethers.getContractFactory("Staking");
+            const contract = await Staking.connect(admin).deploy();
+            const erc20 = await getErc20(admin);
+            await contract.connect(admin).setSupportedToken(erc20.target);
+
+            await expect(
+                contract.connect(staker).stake(1000)
+            ).to.be.revertedWith("ERC20: insufficient allowance");
+        })
+    })
+
+    describe("claim()", () => {
+        it("allows to claim stakes after 30 days", async () => {
+            const [admin, staker] = await ethers.getSigners();
+            const Staking = await ethers.getContractFactory("Staking");
+            const contract = await Staking.connect(admin).deploy();
+            const erc20 = await getErc20(admin);
+            await contract.connect(admin).setSupportedToken(erc20.target);
+            await erc20.connect(admin).transfer(contract.target, 100000);
+            await erc20.connect(admin).transfer(staker.address, 1000);
+            await erc20.connect(staker).increaseAllowance(contract.target, 1000);
+            await contract.connect(staker).stake(1000);
+            await time.increase(3600 * 24 * 30);
+            const reward = await contract.calculateReward(0);
+            await contract.connect(staker).claim(0);
+            const finalBalance = await erc20.balanceOf(staker.address);
+            expect(finalBalance).to.eq(Number(reward) + 1000);
 
         })
     })
